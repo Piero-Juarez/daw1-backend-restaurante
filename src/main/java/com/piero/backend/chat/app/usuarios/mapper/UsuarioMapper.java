03345892c274cd02b797991c1e5fbf2069d3951a
Piero Juarez <juarezpiero@gmail.com>
2025-07-28 00:12:36 -0500

    modificación del toDTO agregando su id a UsuarioDTOResponse y creación de listToDto y toEntity para su uso en UsuarioServiceImpl

--- src/main/java/com/piero/backend/chat/app/usuarios/mapper/UsuarioMapper.java
+++ src/main/java/com/piero/backend/chat/app/usuarios/mapper/UsuarioMapper.java
@@ -1,20 +1,47 @@
 package com.piero.backend.chat.app.usuarios.mapper;
 
-import com.piero.backend.chat.app.usuarios.dto.UsuarioDTO;
+import com.piero.backend.chat.app.usuarios.dto.UsuarioDTORequest;
+import com.piero.backend.chat.app.usuarios.dto.UsuarioDTOResponse;
 import com.piero.backend.chat.app.usuarios.model.Usuario;
+import com.piero.backend.chat.app.usuarios.model.enums.Rol;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@RequiredArgsConstructor
 public class UsuarioMapper {
 
-    public UsuarioDTO toDto(Usuario usuario) {
+    private final PasswordEncoder passwordEncoder;
+
+    public UsuarioDTOResponse toDto(Usuario usuario) {
         if (usuario == null) { return null; }
-        return UsuarioDTO
+        return UsuarioDTOResponse
                 .builder()
+                .id(usuario.getId())
                 .nombre(usuario.getNombre())
                 .apellido(usuario.getApellido())
                 .correo(usuario.getCorreo())
                 .rol(capitalizar(usuario.getRol().name()))
+                .build();
+    }
+
+    public List<UsuarioDTOResponse> listToDto(List<Usuario> usuarios) {
+        if (usuarios.isEmpty()) { return null; }
+        return usuarios.stream().map(this::toDto).toList();
+    }
+
+    public Usuario toEntity(UsuarioDTORequest usuarioDTORequest) {
+        if (usuarioDTORequest == null) { return null; }
+        return Usuario
+                .builder()
+                .nombre(usuarioDTORequest.nombre())
+                .apellido(usuarioDTORequest.apellido())
+                .correo(usuarioDTORequest.correo())
+                .clave(passwordEncoder.encode(usuarioDTORequest.clave()))
+                .rol(Rol.valueOf(usuarioDTORequest.rol()))
                 .build();
     }
 
