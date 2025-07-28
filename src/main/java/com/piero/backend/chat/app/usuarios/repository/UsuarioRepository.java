d9cd05f1b03de90d30ee7e1bf49eb9e8e3205d98
Piero Juarez <juarezpiero@gmail.com>
2025-07-28 00:13:15 -0500

    agregación de nuevas consultas personalizadas

--- src/main/java/com/piero/backend/chat/app/usuarios/repository/UsuarioRepository.java
+++ src/main/java/com/piero/backend/chat/app/usuarios/repository/UsuarioRepository.java
@@ -2,11 +2,23 @@ package com.piero.backend.chat.app.usuarios.repository;
 
 import com.piero.backend.chat.app.usuarios.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    List<Usuario> findAllByActivo(Boolean activo);
+
     Optional<Usuario> findByCorreo(String correo);
+
+    @Modifying
+    @Query("UPDATE usuarios u SET u.activo = false WHERE u.id = :id")
+    void eliminacionLogicaPorId(@Param("id") Integer id);
+
 }
