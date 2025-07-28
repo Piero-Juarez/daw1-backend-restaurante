cbb9c08912d58ac9231570af50084a27e67d78f5
Piero Juarez <juarezpiero@gmail.com>
2025-07-28 00:14:09 -0500

    agregación de nuevas interfaces para la lógica del negocio que usa UsuarioController

--- src/main/java/com/piero/backend/chat/app/usuarios/service/UsuarioService.java
+++ src/main/java/com/piero/backend/chat/app/usuarios/service/UsuarioService.java
@@ -1,9 +1,17 @@
 package com.piero.backend.chat.app.usuarios.service;
 
-import com.piero.backend.chat.app.usuarios.dto.UsuarioDTO;
+import com.piero.backend.chat.app.usuarios.dto.UsuarioDTORequest;
+import com.piero.backend.chat.app.usuarios.dto.UsuarioDTOResponse;
+
+import java.util.List;
 
 public interface UsuarioService {
 
-    UsuarioDTO obtenerUsuarioDtoPorCorreo(String correo);
+    List<UsuarioDTOResponse> obtenerUsuariosDto();
+    UsuarioDTOResponse obtenerUsuarioDtoPorCorreo(String correo);
+    UsuarioDTOResponse obtenerUsuarioDtoPorId(Integer id);
+    UsuarioDTOResponse guardarUsuario(UsuarioDTORequest usuarioDTORequest);
+    UsuarioDTOResponse actualizarUsuario(Integer id, UsuarioDTORequest usuarioDTORequest);
+    void eliminarUsuario(Integer id);
 
 }
