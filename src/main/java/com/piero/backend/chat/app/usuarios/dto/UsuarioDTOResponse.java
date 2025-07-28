17b8dd6f0b2d16802bad216a878ca4abd70da309
Piero Juarez <juarezpiero@gmail.com>
2025-07-28 00:09:57 -0500

    modificación del nombre a UsuarioDTOResponse para una mejor claridad en su uso

--- src/main/java/com/piero/backend/chat/app/usuarios/dto/UsuarioDTOResponse.java
+++ src/main/java/com/piero/backend/chat/app/usuarios/dto/UsuarioDTOResponse.java
@@ -3,7 +3,8 @@ package com.piero.backend.chat.app.usuarios.dto;
 import lombok.Builder;
 
 @Builder
-public record UsuarioDTO(
+public record UsuarioDTOResponse(
+        Integer id,
         String nombre,
         String apellido,
         String correo,
