962a9b269c290ab980ba55ddbb512718361fb817
Piero Juarez <juarezpiero@gmail.com>
2025-07-30 13:25:29 -0500

    Implementación de los tópicos para websocket

--- src/main/java/com/piero/backend/chat/app/platos/controller/CategoriaController.java
+++ src/main/java/com/piero/backend/chat/app/platos/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import com.piero.backend.chat.app.platos.service.CategoriaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,9 +16,10 @@ import java.util.List;
 @RequestMapping("/api/categorias")
 public class CategoriaController {
     private final CategoriaService categoriaService;
+    private final SimpMessagingTemplate messagingTemplate;
     @GetMapping()
     public ResponseEntity<List<CategoriaDTOResponse>> listarCategorias() {
-        return ResponseEntity.ok( categoriaService.listarCategorias());
+        return ResponseEntity.ok(categoriaService.listarCategorias());
     }
     @GetMapping("/{id}")
     public ResponseEntity<CategoriaDTOResponse>obtenerCategoria(@PathVariable Short id) {
@@ -27,16 +29,19 @@ public class CategoriaController {
     @PostMapping()
     public ResponseEntity<CategoriaDTOResponse> guardarCategoria(@RequestBody CategoriaDTORequest categoriaRequest) {
         CategoriaDTOResponse categoriaDTOResponse = categoriaService.guardarCategoria(categoriaRequest);
+        messagingTemplate.convertAndSend("/topic/categorias", categoriaDTOResponse);
         return ResponseEntity.status(HttpStatus.CREATED).body(categoriaDTOResponse);
     }
     @PutMapping("/{id}")
     public ResponseEntity<CategoriaDTOResponse> actualizarCategoria(@PathVariable Short id, @RequestBody CategoriaDTORequest categoriaDTORequest) {
         CategoriaDTOResponse categoriaDtoResponse =  categoriaService.actualizarCategoria(id, categoriaDTORequest);
+        messagingTemplate.convertAndSend("/topic/categorias", categoriaDtoResponse);
         return ResponseEntity.ok(categoriaDtoResponse);
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<?> eliminarCategoria(@PathVariable Short id) {
         categoriaService.eliminarCategoria(id);
-        return ResponseEntity.ok("Se elimino la categoria");
+        messagingTemplate.convertAndSend("/topic/categorias/eliminado", id);
+        return ResponseEntity.noContent().build();
     }
 }
