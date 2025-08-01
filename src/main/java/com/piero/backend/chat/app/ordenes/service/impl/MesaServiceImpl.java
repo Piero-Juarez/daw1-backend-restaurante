c5ff5f4f5167fbcd174b3ddca244b4f14de34fe4
Piero Juarez <juarezpiero@gmail.com>
2025-07-31 20:47:30 -0500

    agregación de una implementación para la interfaz de negocio del entity Mesa

---
+++ src/main/java/com/piero/backend/chat/app/ordenes/service/impl/MesaServiceImpl.java
@@ -0,0 +1,64 @@
+package com.piero.backend.chat.app.ordenes.service.impl;
+
+import com.piero.backend.chat.app.ordenes.dto.mesa.MesaDTORequest;
+import com.piero.backend.chat.app.ordenes.dto.mesa.MesaDTOResponse;
+import com.piero.backend.chat.app.ordenes.mapper.MesaMapper;
+import com.piero.backend.chat.app.ordenes.model.Mesa;
+import com.piero.backend.chat.app.ordenes.model.enums.EstadoMesa;
+import com.piero.backend.chat.app.ordenes.repository.MesaRepository;
+import com.piero.backend.chat.app.ordenes.service.MesaService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MesaServiceImpl implements MesaService {
+
+    private final MesaRepository mesaRepository;
+    private final MesaMapper mesaMapper;
+
+    @Override
+    public List<MesaDTOResponse> listarMesas() {
+        return mesaMapper.listToDto(mesaRepository.findAll());
+    }
+
+    @Override
+    public List<MesaDTOResponse> listarMesasDisponibles() {
+        return mesaMapper.listToDto(mesaRepository.findAllByEstado(EstadoMesa.LIBRE));
+    }
+
+    @Override
+    public MesaDTOResponse guardarMesa(MesaDTORequest mesaDTORequest) {
+        if (mesaDTORequest == null) { return null; }
+        Mesa mesaCreada = mesaRepository.save(mesaMapper.toEntity(mesaDTORequest));
+        return mesaMapper.toDto(mesaCreada);
+    }
+
+    @Override
+    public MesaDTOResponse actualizarMesa(Short id, MesaDTORequest mesaDTORequest) {
+        if (id == null || mesaDTORequest == null) { return null; }
+        Mesa mesaEncontrada = mesaRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Mesa con ID: " + id + ", no encontrada"));
+        // mesaEncontrada.setNumero(mesaDTORequest.numero());
+        mesaEncontrada.setCapacidad(mesaDTORequest.capacidad());
+        if (mesaDTORequest.estado() != null && !mesaDTORequest.estado().isBlank()) {
+            mesaEncontrada.setEstado(EstadoMesa.valueOf(mesaDTORequest.estado()));
+        }
+        return mesaMapper.toDto(mesaRepository.save(mesaEncontrada));
+    }
+
+    @Override
+    public MesaDTOResponse obtenerMesaPorId(Short id) {
+        if (id == null) { return null; }
+        Mesa mesaEncontrada = mesaRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Mesa con ID: " + id + ", no encontrada"));
+        return mesaMapper.toDto(mesaEncontrada);
+    }
+
+    @Override
+    public void eliminarMesa(Short id) {
+        if (id == null) { return; }
+        mesaRepository.deleteById(id);
+    }
+}
