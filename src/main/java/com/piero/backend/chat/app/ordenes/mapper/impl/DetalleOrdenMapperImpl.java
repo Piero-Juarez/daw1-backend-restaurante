4375fc75ab9b5bebb9fe7acf8aa6d89273350aa9
Piero Juarez <juarezpiero@gmail.com>
2025-08-10 17:52:04 -0500

    implementación de la interfaz mapper para DetalleOrden

---
+++ src/main/java/com/piero/backend/chat/app/ordenes/mapper/impl/DetalleOrdenMapperImpl.java
@@ -0,0 +1,23 @@
+package com.piero.backend.chat.app.ordenes.mapper.impl;
+
+import com.piero.backend.chat.app.ordenes.dto.detalleorden.DetalleOrdenResponseDTO;
+import com.piero.backend.chat.app.ordenes.mapper.DetalleOrdenMapper;
+import com.piero.backend.chat.app.ordenes.model.DetalleOrden;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DetalleOrdenMapperImpl implements DetalleOrdenMapper {
+
+    @Override
+    public DetalleOrdenResponseDTO toDetalleResponseDto(DetalleOrden detalleOrden) {
+        return DetalleOrdenResponseDTO.builder()
+                .id(detalleOrden.getId())
+                .nombreItemMenu(detalleOrden.getItemMenu().getNombre())
+                .cantidad(detalleOrden.getCantidad())
+                .precioUnitario(detalleOrden.getPrecioUnitario())
+                .subtotal(detalleOrden.getSubtotal())
+                .total(detalleOrden.getTotal())
+                .build();
+    }
+
+}
