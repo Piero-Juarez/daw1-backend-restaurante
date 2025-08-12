7e87c46a4448896d0510dcbf51660c4d7bd57b7c
Piero Juarez <juarezpiero@gmail.com>
2025-08-12 15:41:50 -0500

    agregación de un nombre adecuado para el JSON de la variable precioMinimo

--- src/main/java/com/piero/backend/chat/app/menu/dto/categoria/CategoriaDTOResponse.java
+++ src/main/java/com/piero/backend/chat/app/menu/dto/categoria/CategoriaDTOResponse.java
@@ -1,8 +1,10 @@
 package com.piero.backend.chat.app.menu.dto.categoria;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record CategoriaDTOResponse (
         Short id,
         String nombre,
         String descripcion,
-        double precioMinimo
+        @JsonProperty("precio_minimo") double precioMinimo
 ){ }
