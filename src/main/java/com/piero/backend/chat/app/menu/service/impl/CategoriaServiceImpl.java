40577e81fb0e788febd8e26e11324a19385b4b62
Piero Juarez <juarezpiero@gmail.com>
2025-08-12 15:43:25 -0500

    cambios en los exceptions personalizados y corrección en la lógica de negocio del método guardarCategoria()

--- src/main/java/com/piero/backend/chat/app/menu/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/piero/backend/chat/app/menu/service/impl/CategoriaServiceImpl.java
@@ -1,13 +1,13 @@
 package com.piero.backend.chat.app.menu.service.impl;
 
 import com.piero.backend.chat.app.exception.BusinessError;
+import com.piero.backend.chat.app.exception.SystemError;
 import com.piero.backend.chat.app.menu.dto.categoria.CategoriaDTORequest;
 import com.piero.backend.chat.app.menu.dto.categoria.CategoriaDTOResponse;
 import com.piero.backend.chat.app.menu.mapper.CategoriaMapper;
 import com.piero.backend.chat.app.menu.model.Categoria;
 import com.piero.backend.chat.app.menu.repository.CategoriaRepository;
 import com.piero.backend.chat.app.menu.service.CategoriaService;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +16,10 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CategoriaServiceImpl implements CategoriaService {
+
     private final CategoriaRepository categoriaRepository;
     private final CategoriaMapper categoriaMapper;
+
     @Override
     public List<CategoriaDTOResponse> listarCategorias() {
         return categoriaMapper.listToDto(categoriaRepository.findAllByActivo(true));
@@ -26,10 +28,10 @@ public class CategoriaServiceImpl implements CategoriaService {
     @Override
     public CategoriaDTOResponse guardarCategoria(CategoriaDTORequest categoriaRequest) {
         if (categoriaRepository.existsCategoriaByNombre(categoriaRequest.nombre())) {
-            throw new BusinessError("El nombre "+categoriaRequest.nombre() + " de la categoría ya existe. Debe ingresar uno nuevo.");
+            throw new SystemError("El nombre "+categoriaRequest.nombre() + " de la categoría ya existe. Debe ingresar uno nuevo.");
         }
-        if(categoriaRequest.precioMinimo()>=0){
-            throw new BusinessError("El precio minimo debe ser mayor que S/ 0");
+        if(categoriaRequest.precioMinimo() <= 0){
+            throw new BusinessError("El precio minimo debe ser mayor que S/0");
         }
         Categoria categoria = categoriaMapper.RequestToEntity(categoriaRequest);
         categoria.setActivo(true);
@@ -38,24 +40,22 @@ public class CategoriaServiceImpl implements CategoriaService {
 
     @Override
     public CategoriaDTOResponse actualizarCategoria(Short id, CategoriaDTORequest categoriaRequest) {
-        Categoria categoria = categoriaRepository.findById(id)
-                .orElseThrow(() -> new BusinessError("Categoria no encontrada con id: " + id));
+        Categoria categoria = categoriaRepository.findById(id).orElseThrow(() -> new SystemError("Categoria no encontrada con id: " + id));
         categoria.setNombre(categoriaRequest.nombre());
         categoria.setDescripcion(categoriaRequest.descripcion());
+        categoria.setPrecioMinimo(categoriaRequest.precioMinimo());
         categoriaRepository.save(categoria);
         return categoriaMapper.toDtoResponse(categoria);
     }
 
     @Override
     public CategoriaDTOResponse obtenerCategoriaPorId(Short id) {
-        return categoriaMapper.toDtoResponse(categoriaRepository.findById(id)
-                .orElseThrow(() -> new BusinessError("Categoria no encontrada con id: " + id))) ;
+        return categoriaMapper.toDtoResponse(categoriaRepository.findById(id).orElseThrow(() -> new SystemError("Categoria no encontrada con id: " + id))) ;
     }
 
     @Override
     public void eliminarCategoria(Short idCategoria) {
-        Categoria categoria = categoriaRepository.findById(idCategoria)
-                .orElseThrow(() -> new BusinessError("Categoria no encontrada con id: " + idCategoria));
+        Categoria categoria = categoriaRepository.findById(idCategoria).orElseThrow(() -> new SystemError("Categoria no encontrada con id: " + idCategoria));
         categoria.eliminarItemMenuAsociado();
         categoriaRepository.save(categoria);
     }
