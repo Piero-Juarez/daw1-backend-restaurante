4743c29819d5a23a0acd5d0464bc02f9494d45f3
Piero Juarez <juarezpiero@gmail.com>
2025-08-16 08:53:06 -0500

    controlador para usar el servicio de jasperreport

---
+++ src/main/java/com/piero/backend/chat/app/reports/ReportController.java
@@ -0,0 +1,29 @@
+package com.piero.backend.chat.app.reports;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reports")
+@RequiredArgsConstructor
+public class ReportController {
+
+    private final ReportService reportService;
+
+    @PostMapping("/boleta/{id}")
+    public ResponseEntity<byte[]> generarBoleta(
+            @PathVariable Long id,
+            @RequestBody InformacionAdicionalBoletaRequest info
+    ) {
+        byte[] report = reportService.generarBoleta("boleta_venta", id, info);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.add("Content-Disposition", "inline; filename=boleta.pdf");
+        return new ResponseEntity<>(report, headers, HttpStatus.OK);
+    }
+
+}
