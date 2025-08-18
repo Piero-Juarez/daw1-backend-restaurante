f9d9c4acd563fe953807c10787e5ee6bcd723690
Piero Juarez <juarezpiero@gmail.com>
2025-08-18 08:24:51 -0500

    eliminación de un bloque de comentario

--- src/main/java/com/piero/backend/chat/app/usuarios/mapper/UsuarioMapper.java
+++ src/main/java/com/piero/backend/chat/app/usuarios/mapper/UsuarioMapper.java
@@ -46,11 +46,4 @@ public class UsuarioMapper {
                 .build();
     }
 
-    /*
-    public static String capitalizar(String texto) {
-        if (texto == null || texto.isBlank()) { return texto; }
-        return texto.substring(0, 1).toUpperCase() + texto.substring(1).toLowerCase();
-    }
-     */
-
 }
