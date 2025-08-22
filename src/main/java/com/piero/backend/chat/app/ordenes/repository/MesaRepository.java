0006c5b3784e7fe03d5de7057eb45a610f001cd1
Piero Juarez <juarezpiero@gmail.com>
2025-08-22 09:51:28 -0500

    nueva query para traer las mesas por estado órden pendiente

--- src/main/java/com/piero/backend/chat/app/ordenes/repository/MesaRepository.java
+++ src/main/java/com/piero/backend/chat/app/ordenes/repository/MesaRepository.java
@@ -2,12 +2,20 @@ package com.piero.backend.chat.app.ordenes.repository;
 
 import com.piero.backend.chat.app.ordenes.model.Mesa;
 import com.piero.backend.chat.app.ordenes.model.enums.EstadoMesa;
+import com.piero.backend.chat.app.ordenes.model.enums.EstadoOrden;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface MesaRepository extends JpaRepository<Mesa, Short> {
+
     List<Mesa> findAllByEstado(EstadoMesa estado);
+
+    @Query("SELECT o.mesa FROM Orden o WHERE o.estado = :estado AND o.activo = true")
+    List<Mesa> encontrarMesasPorEstadoOrden(@Param("estado") EstadoOrden estado);
+
 }
