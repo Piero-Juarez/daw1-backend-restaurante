6a40c2ad3537cd0e9bdc1d5fa84f303a87525a0f
Piero Juarez <juarezpiero@gmail.com>
2025-08-22 10:03:12 -0500

    modificación y nuevos servicios

--- src/main/java/com/piero/backend/chat/app/ordenes/service/OrdenService.java
+++ src/main/java/com/piero/backend/chat/app/ordenes/service/OrdenService.java
@@ -7,13 +7,17 @@ import com.piero.backend.chat.app.ordenes.dto.orden.OrdenResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface OrdenService {
 
     OrdenResponseDTO crearOrden(OrdenRequestDTO ordenRequestDTO);
     OrdenResponseDTO obtenerOrdenPorId(Long id);
+    OrdenResponseDTO obtenerOrdenPendientePorMesa(Short mesaId);
     Page<OrdenResponseDTO> obtenerOrdenes(Pageable pageable);
-    Page<OrdenResponseDTO> obtenerOrdenesDelDia(Pageable pageable, String estadoOrden);
+    Page<OrdenResponseDTO> obtenerOrdenesDelDia(Pageable pageable, List<String> estadosOrden);
     OrdenResponseDTO actualizarOrden(Long ordenId, OrdenActualizarRequestDTO ordenActualizarRequestDTO);
+    OrdenResponseDTO marcarComoPagada(Long ordenId);
     OrdenResponseDTO cambiarEstadoOrden(Long ordenId, OrdenCambiarEstadoRequestDTO ordenCambiarEstadoRequestDTO);
     void desactivarOrdenes();
 
